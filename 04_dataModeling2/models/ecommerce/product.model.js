fb9c8a2b78e2fd32baac8f72d9464bfb7df1044c
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-02-27 19:53:11 +0530

    product model

--- 04_dataModeling2/models/ecommerce/product.model.js
+++ 04_dataModeling2/models/ecommerce/product.model.js
@@ -3,7 +3,35 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
     {
-        
+        description:{
+            required: true,
+            type: String
+        },
+        name:{
+            required: true,
+            type: String
+        },
+        productImage:{
+            type: String  //Images cloud pe upload hoke vahan se url String mein aayega usko yahan par use kr lenege
+
+        },
+        price:{
+            type: Number,
+            default: 0
+        },
+        stock:{
+            default: 0,
+            type: Number
+        },
+        category:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Category",
+            required: true
+        },
+        owner:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
     },
     {
         timestamps: true
