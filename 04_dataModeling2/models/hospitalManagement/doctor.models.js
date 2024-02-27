1c2aa65f36228afe28232604434502f1f6beacad
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-02-27 21:30:21 +0530

    done

--- 04_dataModeling2/models/hospitalManagement/doctor.models.js
+++ 04_dataModeling2/models/hospitalManagement/doctor.models.js
@@ -1,10 +1,31 @@
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
 
 
 const doctorSchema = new mongoose.Schema
 (
     {
-        
+        name:{
+            type: String,
+            required: true
+        },
+        salary:{
+            type: String,
+            required: true,
+        },
+        qualification:{
+            type: String,
+            required: true,
+        },
+        experienceInYears:{
+            type: Number,
+            default: 0
+        },
+        worksInHospitals:[
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Hospital"
+            }
+        ]
     },
     {
         timestamps:true
