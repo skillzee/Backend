2f9647d15d3ecc8b09a1280ddf7574077aac3633
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-02-27 19:45:27 +0530

    commit

---
+++ 04_dataModeling2/models/ecommerce/product.model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+
+const productSchema = new mongoose.Schema(
+    {
+        
+    },
+    {
+        timestamps: true
+    }
+    )
+
+
+
+export const Product = mongoose.model("Product", productSchema)
