481cad07479030fc2240e7406c65ff88a32c280e
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-03-14 19:42:12 +0530

    complete register

--- 05_Project/src/utils/cloudinary.js
+++ 05_Project/src/utils/cloudinary.js
@@ -17,7 +17,8 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto"
         })
         //File has been uploaded successfully
-        console.log("FIle Uploaded Successfully", response.url);
+        // console.log("FIle Uploaded Successfully", response.url);
+        fs.unlinkSync(localFilePath)
         return response
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
